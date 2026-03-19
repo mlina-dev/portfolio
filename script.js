fa4da4570b061787fcb09b4c03ca547aea6b1d1d
Danchou <dvoid367@gmail.com>
2026-03-19 18:12:34 +0800

    ui rework

--- script.js
+++ script.js
@@ -1,59 +1,20 @@
-const Sections = document.querySelectorAll("section");
-const Header = document.querySelector("header");
-const Navigation = document.querySelectorAll(".navigation");
-var lastScrollPosition = window.scrollY;
+// variables and constants
+const NavOptions = document.querySelectorAll(".nav");
 
-// functions
-const hideNavBar = (hide = true) => {
-  // hide navigation bar
-  if (hide == true) {
-    Header.classList.add("hidden_header");
-  } else {
-    Header.classList.remove("hidden_header");
-  }
-};
-
-const activeNavigation = (currentPageId) => {
-  let navId = currentPageId + "_nav";
-  Navigation.forEach((nav) => {
-    nav.classList.remove("active");
-
-    if (nav.id === navId) {
-        nav.classList.add("active");
+// function
+const updateNav = (clicked) => {
+  NavOptions.forEach((nav) => {
+    if (nav == clicked) {
+      nav.classList.add("active_nav");
+    } else {
+      nav.classList.remove("active_nav");
     }
   });
 };
 
-// event listeners
-window.addEventListener("scroll", () => {
-  // when scrolling up show navigation bar
-  var currentScrollPosition = window.scrollY;
-  if (lastScrollPosition > currentScrollPosition) {
-    hideNavBar(false);
-  } else {
-    hideNavBar();
-  }
-  lastScrollPosition = currentScrollPosition;
-});
-
-// observe which section is in display
-const Observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        var section = entry.target;
-        if (section.id === "landing") {
-          hideNavBar(false);
-        }
-
-        activeNavigation(section.id)
-        console.log(Navigation);
-      }
-    });
-  },
-  { threshold: 0.6 },
-); // when target is 60% viewed
-
-Sections.forEach((section) => {
-  Observer.observe(section);
+//  set navigation to active when corresponding section is viewed
+NavOptions.forEach((nav) => {
+  nav.addEventListener("click", (event) => {
+    updateNav(event.currentTarget);
+  });
 });
